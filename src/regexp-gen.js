0fa45262d6fe4c6396ecc3377d660e9ee7319b0a
Mickael van der Beek <mickael@sagacify.com>
2015-02-08 00:05:32 +0100

    Temporary save

--- src/regexp-gen.js
+++ src/regexp-gen.js
@@ -8,6 +8,53 @@ module.exports = (function () {
 
 	RegExpGen.prototype.gen = function (regexp) {
 		var tree = regExpTree.parse(regexp);
+
+		return this.genGroup(regexp);
+	};
+
+	RegExpGen.prototype.genGroup = function (prevGenerated, group) {
+		var sequences = group.sequences;
+		var currGenerated = [];
+
+		if (typeof sequences === 'string') {
+			currGenerated.push(sequences);
+		}
+		else {
+			var sequence;
+
+			for (var i = 0, len = sequences.length; i < len; i++) {
+				sequence = sequences[i];
+
+				if (sequence.isGroup === true) {
+					currGenerated.push(
+						this.genGroup(prevGenerated, currGenerated)
+					);
+				}
+				else if (sequence.isSet === true) {
+					currGenerated.push(
+						this.genSet(prevGenerated, currGenerated)
+					);
+				}
+			}
+		}
+
+		return this.mergeGen(prevGenerated, currGenerated);
+	};
+
+	RegExpGen.prototype.genSet = function (prevGenerated, set) {
+		
+	};
+
+	RegExpGen.prototype.mergeGen = function (prevGenerated, currGenerated) {
+		var generated = [];
+
+		for (var i = 0, lenPrev = prevGenerated.length; i < lenPrev; i++) {
+			for (var k = 0, lenCurr = currGenerated.length; k < lenCurr; k++) {
+				generated.push(prevGenerated[i] + currGenerated[k]);
+			}
+		}
+
+		return generated;
 	};
 
 	return RegExpGen;
