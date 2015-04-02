c3c40c0bb7178fa1d282f8d801de38d976567ae6
Mickael van der Beek <mickael@sagacify.com>
2015-04-02 23:49:25 +0200

    Quick proof-of-concept.

--- src/regexp-gen.js
+++ src/regexp-gen.js
@@ -4,57 +4,121 @@ var regExpTree = new RegExpTree();
 module.exports = (function () {
 	'use strict';
 
-	function RegExpGen () {}
+	function RegExpGen () {
+		this.isNumber = /^([0-9]+)$/;
+
+		this.currGenerated = [];
+	}
 
 	RegExpGen.prototype.gen = function (regexp) {
 		var tree = regExpTree.parse(regexp);
 
-		return this.genGroup(regexp);
+		console.log('TREE=', require('util').inspect(tree, {
+			colors: true,
+			depth: 10
+		}));
+
+		this.genGroup(tree);
+
+		var combinations = this.genCombinations([''], 0, this.currGenerated);
+
+		return combinations;
 	};
 
-	RegExpGen.prototype.genGroup = function (prevGenerated, group) {
+	RegExpGen.prototype.genGroup = function (group) {
 		var sequences = group.sequences;
-		var currGenerated = [];
+		var generated;
 
-		if (typeof sequences === 'string') {
-			currGenerated.push(sequences);
+		if (!Array.isArray(sequences)) {
+			generated = this.genStringBoundaries(sequences);
+
+			this.currGenerated.push(generated);
 		}
-		else {
+		else {
 			var sequence;
 
 			for (var i = 0, len = sequences.length; i < len; i++) {
 				sequence = sequences[i];
+				generated = null;
 
 				if (sequence.isGroup === true) {
-					currGenerated.push(
-						this.genGroup(prevGenerated, currGenerated)
-					);
+					this.genGroup(sequence);
 				}
 				else if (sequence.isSet === true) {
-					currGenerated.push(
-						this.genSet(prevGenerated, currGenerated)
-					);
+					generated = this.genSet(sequence);
+				}
+
+				if (generated) {
+					this.currGenerated.push(generated);
 				}
 			}
 		}
-
-		return this.mergeGen(prevGenerated, currGenerated);
 	};
 
-	RegExpGen.prototype.genSet = function (prevGenerated, set) {
-		
+	RegExpGen.prototype.genSet = function (set) {
+		var me = this;
+
+		return set.ranges.reduce(function (boundaries, range) {
+			if (me.isNumber.test(range[0]) && me.isNumber.test(range[1])) {
+				boundaries = boundaries
+					.concat(
+						range[0] ? me.genNumberBoundaries(range[0]) : [],
+						range[1] ? me.genNumberBoundaries(range[1]) : []
+					);
+			}
+			else {
+				boundaries = boundaries
+				.concat(
+					range[0] ? me.genStringBoundaries(range[0]) : [],
+					range[1] ? me.genStringBoundaries(range[1]) : []
+				);			
+			}
+
+			return boundaries;
+		}, []);
 	};
 
-	RegExpGen.prototype.mergeGen = function (prevGenerated, currGenerated) {
-		var generated = [];
+	RegExpGen.prototype.genStringBoundaries = function (string) {
+		return [
+			string.slice(0, -1) + String.fromCharCode(string.slice(-1).charCodeAt(0) - 1),
+			string,
+			string.slice(0, -1) + String.fromCharCode(string.slice(-1).charCodeAt(0) + 1),
+		];
+	};
 
-		for (var i = 0, lenPrev = prevGenerated.length; i < lenPrev; i++) {
-			for (var k = 0, lenCurr = currGenerated.length; k < lenCurr; k++) {
-				generated.push(prevGenerated[i] + currGenerated[k]);
+	RegExpGen.prototype.genNumberBoundaries = function (number) {
+		return [
+			String(parseInt(number, 10) - 1),
+			String(number),
+			String(parseInt(number, 10) + 1),
+		];
+	};
+
+	RegExpGen.prototype.genCombinations = function (combinations, index, group) {
+		if (index === group.length) {
+			return combinations;
+		}
+
+		var newCombinations = [];
+		var combination;
+		var boundaries;
+		var boundary;
+
+		for (var i = 0, ilen = combinations.length; i < ilen; i++) {
+			combination = combinations[i];
+
+			boundaries = group[index];
+
+			for (var k = 0, klen = boundaries.length; k < klen; k++) {
+				boundary = boundaries[k];
+
+				newCombinations.push(
+					combination + boundary
+				);
 			}
 		}
 
-		return generated;
+		return this.genCombinations(newCombinations, index + 1, group);
 	};
 
 	return RegExpGen;
---
+++ test/test.js
@@ -0,0 +1,15 @@
+var RegExpGen = require('../src/regexp-gen');
+// var regExpGen = new RegExpGen();
+
+var assert = require('assert');
+
+// console.log(regExpGen.gen(/[a-c](def)/));
+// console.log(regExpGen.currGenerated);
+
+// console.log(regExpGen.gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/));
+// console.log('LEN=', regExpGen.gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/).length);
+
+console.log((new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/));
+console.log('LEN=', (new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/).length);
+
+// console.log(regExpGen.currGenerated);
