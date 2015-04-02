c39a0f08f9b467e9f6301bd1089590aaaa9ad886
Mickael van der Beek <mickael@sagacify.com>
2015-04-03 00:02:22 +0200

    Some other tests.

--- test/test.js
+++ test/test.js
@@ -9,7 +9,10 @@ var assert = require('assert');
 // console.log(regExpGen.gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/));
 // console.log('LEN=', regExpGen.gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/).length);
 
-console.log((new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/));
-console.log('LEN=', (new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/).length);
+// console.log((new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/));
+// console.log('LEN=', (new RegExpGen()).gen(/[a-zA-Z0-9_\.\-]@[a-zA-Z0-9\-]\.[a-zA-Z0-9]/).length);
+
+console.log((new RegExpGen()).gen(/abc[0-9]/));
+console.log('LEN=', (new RegExpGen()).gen(/abc[0-9]/).length);
 
 // console.log(regExpGen.currGenerated);
